3930a4c4d32cd965a4ed41f13dc61ffa7548088c
at0m741 <louistouz@icloud.com>
2024-12-21 12:14:06 +0100

    Better

--- memcpy.c
+++ memcpy.c
@@ -21,20 +21,19 @@ void *_memcpy_avx(void *dest, const void *src, size_t len) {
     __m256i *d = (__m256i *)cdest;
 
     for (size_t i = 0; i < num_unrolled; ++i) {
-		if (len <= 256)
-		{
-			_mm_prefetch((const char *)(s + 8), _MM_HINT_T0);
-			_mm_prefetch((const char *)(d + 8), _MM_HINT_T0);
-		}
-		_mm256_storeu_si256(d++, _mm256_loadu_si256(s++));
+	if (len <= 256){
+		_mm_prefetch((const char *)(s + 8), _MM_HINT_T0);
+		_mm_prefetch((const char *)(d + 8), _MM_HINT_T0);
+	}
+	_mm256_storeu_si256(d++, _mm256_loadu_si256(s++));
         _mm256_storeu_si256(d++, _mm256_loadu_si256(s++));
         _mm256_storeu_si256(d++, _mm256_loadu_si256(s++));
         _mm256_storeu_si256(d++, _mm256_loadu_si256(s++));
         _mm256_storeu_si256(d++, _mm256_loadu_si256(s++));
         _mm256_storeu_si256(d++, _mm256_loadu_si256(s++));
-		_mm256_storeu_si256(d++, _mm256_loadu_si256(s++));
+	_mm256_storeu_si256(d++, _mm256_loadu_si256(s++));
         _mm256_storeu_si256(d++, _mm256_loadu_si256(s++));
-	}	
+    }	
     for (size_t i = 0; i < remainder_blocks; ++i) {
         _mm256_storeu_si256(d++, _mm256_loadu_si256(s++));
     }
@@ -64,7 +63,7 @@ void benchmark_memcpy(size_t size, size_t iterations) {
     uint8_t *dest_custom = (uint8_t *)aligned_alloc(32, size);
 
     if (!src || !dest_std || !dest_custom) {
-        fprintf(stderr, "Erreur d'allocation mémoire\n");
+        fprintf(stderr, "Error alloc\n");
         exit(EXIT_FAILURE);
     }
 
@@ -90,9 +89,9 @@ void benchmark_memcpy(size_t size, size_t iterations) {
     time_custom = end - start;
 
     if (memcmp(dest_std, dest_custom, size) != 0) {
-        fprintf(stderr, "Erreur: Les résultats de memcpy et _memcpy_avx diffèrent pour la taille %zu\n", size);
+        fprintf(stderr, "Error: different results of memcpy and _memcpy_avx for sizeof %zu\n", size);
     } else {
-        printf("Taille: %8zu bytes | memcpy: %8.6f s | _memcpy_avx: %8.6f s | Gain: %.2f%%\n",
+        printf("Size: %8zu bytes | memcpy: %8.6f s | _memcpy_avx: %8.6f s | Gain: %.2f%%\n",
                size, time_std, time_custom,
                ((time_std - time_custom) / time_std) * 100);
     }
@@ -110,9 +109,9 @@ int main() {
 
     size_t iterations = 100000;
 
-    printf("Benchmark de memcpy vs _memcpy_avx\n");
-    printf("Iterations: %zu\n\n", iterations);
-    printf("Taille (bytes) | memcpy (s) | _memcpy_avx (s) | Gain\n");
+    printf("Benchmark of memcpy vs _memcpy_avx\n");
+    printf("Iter: %zu\n\n", iterations);
+    printf("Size (bytes) | memcpy (s) | _memcpy_avx (s) | Gain\n");
     printf("---------------------------------------------------------\n");
 
     for (size_t i = 0; i < num_sizes; ++i) {
